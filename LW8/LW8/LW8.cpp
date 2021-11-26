d5ea40cf02f34e190c829e566a11f5ef50b92fbd
Nouhelgod <36785937+Nouhelgod@users.noreply.github.com>
2021-11-26 16:02:52 +0600

    Готово

---
+++ LW8/LW8/LW8.cpp
@@ -0,0 +1,43 @@
+﻿// Сурков Д. А. 8В01
+// ЛР 8 Вариант 29
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+string PATH = "D:\\Docks\\C++ scripts\\LW8\\LW8\\nums.txt";
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int i = 0;
+    char sim;
+    char last = 'e';
+
+    ifstream orig(PATH);
+    if (orig.is_open()) {
+        while (orig.get(sim)) {
+            cout << sim;
+        }
+        cout << endl;
+        orig.close();
+    }
+
+
+    ifstream mod(PATH);
+    if (mod.is_open()) {
+        while (mod.get(sim)) {
+            if (sim != last) {
+                cout << sim;
+                last = sim;
+            }
+            else {
+                last = sim;
+            }
+            i++;
+        }
+        mod.close();
+    }
+}
---
+++ LW9/LW9/LW9.cpp
@@ -0,0 +1,37 @@
+﻿// Сурков Д. А. 8В01
+// ЛР 9 Вариант 30
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+double f(double x) {
+    return(x / (pow(x, 4) + 3 * pow(x, 2) + 2));
+}
+
+
+double s(double f(double), int a, int b, double n = 0.1) {
+    double total_s = 0;
+    double h = 0;
+
+    for (double i = a; i <= b; i += n) {
+        h = f(i);
+        double s = h * n;
+        total_s += s;
+    }
+
+    return total_s;
+}
+
+
+int main()
+{
+    double a = 1.0;
+    double b = 2.0;
+
+    cout << "1) " << s(f, a, b) << endl;
+    cout << "2) " << s(f, a, b, 0.00000001) << endl;
+    cout << "3) " << f(b) - f(a) << endl;
+}
+
